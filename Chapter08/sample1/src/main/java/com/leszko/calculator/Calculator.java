c9f8d5f55948ec4101153cf499039826a9b838ff
Ei <ebtrampe@gmail.com>
2021-10-02 19:24:04 +0800

    Add Javadoc

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,6 +3,10 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+/**
+@author Ei
+@version 1
+*/
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
